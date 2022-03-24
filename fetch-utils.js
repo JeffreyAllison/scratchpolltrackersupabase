dc983bd560531a4ee83d9470334f96bdb0b8cd48
JeffAllison <itsjallison@gmail.com>
2022-03-24 10:44:20 -0700

    created render, fetch, polls.html, polls.js, added API key to fetch utils and added SB CDN to html files

---
+++ fetch-utils.js
@@ -0,0 +1,5 @@
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxyYnpocGxkanJ4cWtqc2tjaXpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDc1NTIwMDMsImV4cCI6MTk2MzEyODAwM30.idE1m2ehmckSIic7mOSaXFl1McMzBdIrhU_Vrsr6UyI'
+EXAMPLE USAGE
+const SUPABASE_URL = "https://lrbzhpldjrxqkjskcizc.supabase.co"
+
+const supabase = createClient(SUPABASE_URL, process.env.SUPABASE_KEY);
