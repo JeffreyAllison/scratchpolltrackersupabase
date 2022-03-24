b775a1efca852bbf75ca24fd72afca51f1989925
JeffAllison <itsjallison@gmail.com>
2022-03-24 10:52:35 -0700

    changed API language in fetch.js

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,5 +1,5 @@
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxyYnpocGxkanJ4cWtqc2tjaXpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDc1NTIwMDMsImV4cCI6MTk2MzEyODAwM30.idE1m2ehmckSIic7mOSaXFl1McMzBdIrhU_Vrsr6UyI'
-EXAMPLE USAGE
-const SUPABASE_URL = "https://lrbzhpldjrxqkjskcizc.supabase.co"
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxyYnpocGxkanJ4cWtqc2tjaXpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDc1NTIwMDMsImV4cCI6MTk2MzEyODAwM30.idE1m2ehmckSIic7mOSaXFl1McMzBdIrhU_Vrsr6UyI';
 
-const supabase = createClient(SUPABASE_URL, process.env.SUPABASE_KEY);
+const SUPABASE_URL = 'https://lrbzhpldjrxqkjskcizc.supabase.co';
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
