635173c9420632b724ec1b86617bc9729f8efb4f
JeffAllison <itsjallison@gmail.com>
2022-03-24 14:29:13 -0700

    added button classes in polls html and added js elements in polls js

--- polls/polls.js
+++ polls/polls.js
@@ -4,6 +4,10 @@ const pastPollsEl = document.querySelector('.previous-polls-list');
 const currentQuestionEl = document.querySelector('.current-question-text');
 const currentOption1El = document.querySelector('.option-1-text');
 const currentOption2El = document.querySelector('.option-2-text');
+const option1AddVoteButton = document.querySelector('.option-1-add-vote');
+const option1MinusVoteButton = document.querySelector('.option-1-minus-vote');
+const option2AddVoteButton = document.querySelector('.option-2-add-vote');
+const option2MinusVoteButton = document.querySelector('.option-2-minus-vote');
 const formEl = document.querySelector('form');
 
 let currentPollQuestion = '';
@@ -27,3 +31,9 @@ formEl.addEventListener('submit', (e) => {
 
   formEl.reset();
 });
+
+function displayCurrentQuestion () {
+  currentQuestionEl.textContent = currentPollQuestion;
+  currentOption1El.textContent = currentOption1;
+  currentOption2El.textContent = currentOption2;
+}
